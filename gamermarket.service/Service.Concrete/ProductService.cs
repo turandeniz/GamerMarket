76f611a9f279b8c8a5c61ebd86cf63e2b9f8d14b
turandeniz <49750755+turandeniz@users.noreply.github.com>
2019-04-18 13:09:58 +0300

    Initial commit

---
+++ gamermarket.service/Service.Concrete/ProductService.cs
@@ -0,0 +1,16 @@
+﻿using GamerMarket.Model.Model.Entities;
+using GamerMarket.Service.Service.Abstract;
+using GamerMarket.Core.Core.Entity.Enums;
+using System.Collections.Generic;
+
+namespace GamerMarket.Service.Service.Concrete
+{
+    public class ProductService:BaseService<Product>
+    {
+        public int ProductCount() => GetDefault((x => x.Status == Status.Active || x.Status == Status.Updated)).Count;
+
+        public List<Product> GetWebProducts() => GetDefault(x => (x.Status == Status.Active || x.Status == Status.Updated) && x.DisplayOnWeb == true);
+
+        public List<Product> GetMobileProducts() => GetDefault(x => (x.Status == Status.Active || x.Status == Status.Updated) && x.DisplayOnMobile == true);
+    }
+}
---
+++ gamermarket.ui/areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,80 @@
+﻿using GamerMarket.Model.Model.Entities;
+using GamerMarket.Service.Service.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GamerMarket.UI.Areas.Admin.Controllers
+{
+    public class OrderController : BaseController
+    {
+        OrderService _orderService;
+        OrderDetailService _orderDetailService;
+        ProductService _productService;
+
+        public OrderController()
+        {
+            _orderService = new OrderService();
+            _orderDetailService = new OrderDetailService();
+            _productService = new ProductService();
+        }
+
+        public ActionResult List()
+        {
+            List<Order> Orders = _orderService.ListPendingOrders();
+            if (Orders.Count!=0)
+            {
+                return View(_orderService.ListPendingOrders());
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            
+        }
+
+        public ActionResult Detail(Guid id)
+        {
+            List<OrderDetail> OrderDetails = _orderDetailService.GetDefault(x => x.OrderID == id);
+            if (OrderDetails.Count != 0)
+            {
+                return View(OrderDetails);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+          
+        }
+
+        public ActionResult ConfirmOrder(Guid id)
+        {
+            Order order = _orderService.GetById(id);
+
+            //Stoktan Düşme İşlemi. Her bir sipariş detayda gezilir ve içindeki ürün yakalanır. Sonrasında stoklardan siparişteki miktar kadar düşülür.
+            foreach (var item in order.OrderDetails)
+            {
+                Product p = _productService.GetById(item.ProductID);
+                p.UnitsInStock -= item.Quantity;
+                _productService.Update(p);
+            }
+
+            order.OrderStatus = OrderStatus.Completed;
+            _orderService.Update(order);
+
+            return RedirectToAction("List");
+
+        }
+
+        public ActionResult RejectOrder(Guid id)
+        {
+            Order o = _orderService.GetById(id);
+            o.OrderStatus = OrderStatus.Declined;
+            _orderService.Remove(o);
+            return RedirectToAction("List");
+        }
+    }
+}
